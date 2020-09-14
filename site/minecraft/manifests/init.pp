2c1bb35857dd4329d570e6e6efded90f6d09b8f9
superpcm <70666425+superpcm@users.noreply.github.com>
2020-09-14 16:36:33 -0400

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,21 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  
+}
